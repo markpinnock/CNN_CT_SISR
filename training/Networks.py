806ba7688f362598e6ff6101a7d6915e17640b0b
Mark Pinnock <rmappin@ucl.ac.uk>
2020-01-14 12:04:00 +0000

    Removed conv layer from each block, changed output conv to 1x1x1

--- training/Networks.py
+++ training/Networks.py
@@ -6,24 +6,20 @@ import tensorflow.keras as keras
 def dnResNetBlock(nc, inputlayer):
     conv1 = keras.layers.Conv3D(nc, (3, 3, 3), strides=(1, 1, 1), padding='same', activation='relu')(inputlayer)
     # BN1 = keras.layers.BatchNormalization()(conv1)
-    conv2 = keras.layers.Conv3D(nc, (3, 3, 3), strides=(1, 1, 1), padding='same', activation='relu')(conv1)
+    conv2 = keras.layers.Conv3D(nc, (3, 3, 3), strides=(2, 2, 1), padding='same', activation='relu')(conv1)
     # BN2 = keras.layers.BatchNormalization()(conv2)
-    # skip = conv2
-    pool = keras.layers.Conv3D(nc, (2, 2, 1), strides=(2, 2, 1), padding='same', activation='relu')(conv2)
     
-    return conv2, pool
+    return conv1, conv2
 
 
 def upResNetBlock(nc, inputlayer, skip, tconv_strides):
     tconv = keras.layers.Conv3DTranspose(nc, (2, 2, 2), strides=tconv_strides, padding='same', activation='relu')(inputlayer)
     # BN1 = keras.layers.BatchNormalization()(tconv)
     concat = keras.layers.concatenate([tconv, skip], axis=4)
-    conv1 = keras.layers.Conv3D(nc, (3, 3, 3), strides=(1, 1, 1), padding='same', activation='relu')(concat)
+    conv = keras.layers.Conv3D(nc, (3, 3, 3), strides=(1, 1, 1), padding='same', activation='relu')(concat)
     # BN2 = keras.layers.BatchNormalization()(conv1)
-    conv2 = keras.layers.Conv3D(nc, (3, 3, 3), strides=(1, 1, 1), padding='same', activation='relu')(conv1)
-    # BN3 = keras.layers.BatchNormalization()(conv2)
     
-    return conv2
+    return conv
 
 
 def upSampModule(skip_in, nc, up_factor):
@@ -57,6 +53,6 @@ def UNetGen(input_shape, starting_channels):
     upres2 = upResNetBlock(nc * 2, upres3, upSampModule(skip2, nc * 2, 1), (2, 2, 2))
     upres1 = upResNetBlock(nc, upres2, upSampModule(skip1, nc, 2), (2, 2, 2))
 
-    outputlayer = keras.layers.Conv3D(1, (3, 3, 3), strides=(1, 1, 1), padding='same', activation='sigmoid')(upres1)
+    outputlayer = keras.layers.Conv3D(1, (1, 1, 1), strides=(1, 1, 1), padding='same', activation='sigmoid')(upres1)
 
     return keras.Model(inputs=inputlayer, outputs=outputlayer)
